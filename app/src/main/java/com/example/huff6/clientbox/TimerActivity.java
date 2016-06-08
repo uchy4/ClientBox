91c976fb6e722e4e10b5c8423cd6f8115e9d70eb
uchy4 <uch14002@byui.edu>
2016-06-08 11:00:35 -0600

    updated build.gradle and implemented TimerActivity Test

--- app/src/main/java/com/example/huff6/clientbox/TimerActivity.java
+++ app/src/main/java/com/example/huff6/clientbox/TimerActivity.java
@@ -5,9 +5,19 @@ import android.os.Bundle;
 
 public class TimerActivity extends AppCompatActivity {
 
+    int time = 60;
+    Boolean check = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+    }
+
+    Boolean getTimer(){
+        return check;
+    }
+
+    int getValue(){
+        return time;
     }
 }
--- app/src/test/java/com/example/huff6/clientbox/TimerActivityTest.java
+++ app/src/test/java/com/example/huff6/clientbox/TimerActivityTest.java
@@ -2,6 +2,8 @@ package com.example.huff6.clientbox; /**
  * Created by joshu on 6/8/2016.
  */
 
+import android.content.Context;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +12,24 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class TimerActivityTest {
+
+    TimerActivity myObjectUnderTest = new TimerActivity();
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void onClickManualEntry() throws Exception {
+        // ...when the int is returned from the object under test...
+        int result = myObjectUnderTest.getValue();
+        assertNotNull(result);
+    }
+
+
+    @Test
+    public void onClickStartStop() throws Exception {
+
+        // ...when the bool is returned from the object under test...
+        Boolean result = myObjectUnderTest.getTimer();
+
+        // ...then the result should be the expected one.
+        assertTrue(result);
     }
 }
