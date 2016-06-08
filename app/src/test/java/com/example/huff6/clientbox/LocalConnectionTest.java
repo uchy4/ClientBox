2ca162c8cf7a24b6193165b796c51295b72eb5cf
tslemke11 <tyleris@gmail.com>
2016-06-08 12:13:03 -0600

    localConnectionTest update

--- app/src/test/java/com/example/huff6/clientbox/LocalConnectionTest.java
+++ app/src/test/java/com/example/huff6/clientbox/LocalConnectionTest.java
@@ -1,7 +1,28 @@
 package com.example.huff6.clientbox;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Tyler on 6/8/2016.
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class LocalConnectionTest {
+
+    LocalConnection myObjectUnderTest = new LocalConnection();
+
+    @Test
+    public void updateQueueTest() throws Exception {
+        // ...when the int is returned from the object under test...
+        assertTrue(myObjectUnderTest.getLogs().isEmpty());
+    }
+
+
+    @Test
+    public void haveInternetConnectionTest() throws Exception {
+        Boolean result = myObjectUnderTest.getServerConnection().haveInternetConnection();
+        assertTrue(result);
+    }
 }
