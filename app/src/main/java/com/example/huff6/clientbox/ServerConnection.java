fd99a840334c6416680fe76d6fa8aa21add1a0c1
tslemke11 <tyleris@gmail.com>
2016-06-08 10:33:56 -0600

    commiting two tests and Server Connection Stub

--- app/src/main/java/com/example/huff6/clientbox/ServerConnection.java
+++ app/src/main/java/com/example/huff6/clientbox/ServerConnection.java
@@ -1,7 +1,32 @@
 package com.example.huff6.clientbox;
 
+import java.util.ArrayList;
+
 /**
  * Created by joshu on 6/6/2016.
  */
 public class ServerConnection {
+
+    private String username;
+    private String number;
+
+    public String retreiveClient(String id){
+        return "";
+    }
+
+    public void writeClient(String id){
+
+    }
+
+    public ArrayList<String> retrieveLog(String id){
+        ArrayList<String> temp = null;
+
+        return temp;
+    }
+
+    public ArrayList<String> retrieveClient(){
+        ArrayList<String> temp = null;
+
+        return temp;
+    }
 }
---
+++ app/src/test/java/com/example/huff6/clientbox/LoginActivityTest.java
@@ -0,0 +1,7 @@
+package com.example.huff6.clientbox;
+
+/**
+ * Created by Tyler on 6/8/2016.
+ */
+public class LoginActivityTest {
+}
