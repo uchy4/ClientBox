19d928bca61e4e5079d05a05ef5f595632fa5bb7
uchy4 <uch14002@byui.edu>
2016-06-18 16:55:01 -0600

    added an update call log to main

--- app/src/main/java/com/example/huff6/clientbox/MainActivity.java
+++ app/src/main/java/com/example/huff6/clientbox/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.huff6.clientbox;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.provider.CallLog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     public static final String MESSAGE2 = "com.example.huff6.clientbox.TimerActivity";
     public static final String MESSAGE3 = "com.example.huff6.clientbox.ManualEntryActivity";
     public static final String MESSAGE4 = "com.example.huff6.clientbox.ClientLookupActivity";
+
+    TextView call;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(LoginActivity.EXTRA_MESSAGE);
+
+        call = (TextView) findViewById(R.id.call);
+        updateCallInfo();
     }
 
     public void goToAddClient(View v){
@@ -77,5 +89,43 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    void updateCallInfo(){
+        try {
+            Context context;
+            StringBuffer sb = new StringBuffer();
+            Cursor managedCursor =  managedQuery(CallLog.Calls.CONTENT_URI, null, null, null, null);
+            int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
+            int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
+            int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
+            int duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
+            sb.append("Call Details :");
+            while (managedCursor.moveToNext()) {
+                String phNumber = managedCursor.getString(number);
+                String callType = managedCursor.getString(type);
+                String callDate = managedCursor.getString(date);
+                Date callDayTime = new Date(Long.valueOf(callDate));
+                String callDuration = managedCursor.getString(duration);
+                String dir = null;
+                int dircode = Integer.parseInt(callType);
+                switch (dircode) {
+                    case CallLog.Calls.OUTGOING_TYPE:
+                        dir = "OUTGOING";
+                        break;
 
+                    case CallLog.Calls.INCOMING_TYPE:
+                        dir = "INCOMING";
+                        break;
+
+                    case CallLog.Calls.MISSED_TYPE:
+                        dir = "MISSED";
+                        break;
+                }
+                sb.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- " + dir + " \nCall Date:--- " + callDayTime + " \nCall duration in sec :--- " + callDuration);
+                sb.append("\n----------------------------------");
+            }
+            //sb is the call information
+            managedCursor.close();
+            call.setText("updated");
+        } catch (Exception e){}
+    }
 }
